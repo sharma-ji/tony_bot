58b651a09f5dfd7832d1bd99b992c102dc525e8f
Mukul Sharma <mukulsharma.0462@gmail.com>
2017-10-25 00:01:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import aiml
 kernel= aiml.Kernel()
-kernel.bootstrap(brainFile = "/home/pi/chat_bot/bot_brain.brn")
+kernel.bootstrap(brainFile = "bot_brain.brn")
 kernel.setBotPredicate("hometown", "127.0.0.1")
 kernel.setBotPredicate("name", "Tony")
 kernel.setBotPredicate("master", "Sharmaji")
