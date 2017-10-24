013351bf94ce05d1666f6e0fd28562dda2dbeb87
Mukul Sharma <mukulsharma.0462@gmail.com>
2017-10-25 00:06:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,13 @@
+#importing the aiml module
 import aiml
-kernel= aiml.Kernel()
-kernel.bootstrap(brainFile = "bot_brain.brn")
-kernel.setBotPredicate("hometown", "127.0.0.1")
+kernel= aiml.Kernel() #initiating the Kernel
+kernel.bootstrap(brainFile = "bot_brain.brn") #since I have already optimised the files for you so now you need to only include the brain file
+'''set predicates for your bot name, hometown, gender and so on.............
+ alo you can set the user predicted by using 
+ kernel.setBotPredicate()
+ And you can set the sessionID for custom chat behaviour'''
+
+kernel.setBotPredicate("hometown", "127.0.0.1") 
 kernel.setBotPredicate("name", "Tony")
 kernel.setBotPredicate("master", "Sharmaji")
 kernel.setBotPredicate("gender", "Male")
